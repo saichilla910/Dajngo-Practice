d6ccf989b61f4df286d4f8a58c1aca898b5fe4c6
saichilla <saichilla910@gmail.com>
2026-08-26 21:22:34 +0530

    fb's implemethed for manging the CURD

--- Products/urls.py
+++ Products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views as product_view
 urlpatterns=[
-    path('all/',product_view.Products.as_view(),name="products"),
-    path('<int:pk>/',product_view.Products.as_view(),name="detail"),
-    path('create/',product_view.Products.as_view(),name="create"),
+    path('all/',product_view.Products,name="products"),
+    path('<int:pk>/',product_view.Products,name="detail"),
+    path('create/',product_view.Products,name="create"),
+    path('delete/<int:pk>/',product_view.product_detail,name="delete"),
+    path('update/<int:pk>/',product_view.product_detail,name="update"),
 ]
--- Products/views.py
+++ Products/views.py
@@ -4,19 +4,45 @@ from .models import Product
 from .serializers import product_serializer
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-class Products(APIView):
+# class Products(APIView):
 
-    def get(self, request,pk=None):
+#     def get(self, request,pk=None):
+#         if pk is not None:
+#             product=get_object_or_404(Product,pk=pk)
+#             serializer = product_serializer(product, many=False)
+#         else:
+#             products = Product.objects.all()
+#             serializer = product_serializer(products, many=True)
+#         return Response(serializer.data)
+#     def post(self,request):
+#         parser_classes = [MultiPartParser, FormParser]
+#         serializer=product_serializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             title=serializer.validated_data.get("Name")
+#             description=serializer.validated_data.get("description")
+#             if description is None:
+#                 description=title
+#             serializer.save()
+#             return Response(serializer.data)
+
+
+            
+##### function based View for managing the CURD #####################
+@api_view(['GET','POST'])
+def Products(request,pk=None):
+    method=request.method
+    if method=='GET':
         if pk is not None:
             product=get_object_or_404(Product,pk=pk)
             serializer = product_serializer(product, many=False)
-        else:
-            products = Product.objects.all()
-            serializer = product_serializer(products, many=True)
+            return Response(serializer.data)
+        products=Product.objects.all()
+        serializer=product_serializer(products,many=True)
         return Response(serializer.data)
-    def post(self,request):
-        parser_classes = [MultiPartParser, FormParser]
+    if method=='POST':
         serializer=product_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             title=serializer.validated_data.get("Name")
@@ -25,4 +51,28 @@ class Products(APIView):
                 description=title
             serializer.save()
             return Response(serializer.data)
-            
+
+@api_view(["PUT", "PATCH", "DELETE"])
+def product_detail(request, pk):
+
+    product = get_object_or_404(Product, id=pk)
+
+    # DELETE
+    if request.method == "DELETE":
+        product.delete()
+        return Response(
+            {"message": "Product deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+    # UPDATE
+    serializer = product_serializer(
+        product,
+        data=request.data,
+        partial=(request.method == "PATCH")
+    )
+
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data)
